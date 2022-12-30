856ca3f310c2b8cde5c8908753afff9319f5a65f
hyeg <hyeg0121@naver.com>
2022-12-30 13:38:57 +0900

    마우스 클릭기능 추가

--- mousegame/main.cpp
+++ mousegame/main.cpp
@@ -11,22 +11,29 @@ int main(void) {
 	window.setFramerateLimit(60);
 
 	Vector2i mouse_pos;
+	int click_cnt = 0;
 
 	while (window.isOpen())
 	{
-		mouse_pos = Mouse::getPosition();
+		mouse_pos = Mouse::getPosition(window);
 		Event event;
 		while (window.pollEvent(event))
 		{
 			switch (event.type)
 			{
-			case Event::Closed: 
-				window.close();
-				break;
+				case Event::Closed: 
+					window.close();
+					break;
+				//한 번 누르면 한 번만 적용
+				case Event::MouseButtonPressed: 
+					if (event.mouseButton.button == Mouse::Left) {
+						click_cnt++;
+					}
+
 			}//switch
 		}//event
 
-		printf("(%d, %d)\n", mouse_pos.x, mouse_pos.y);
+		printf("(%d, %d), click_cnt : %d\n", mouse_pos.x, mouse_pos.y, click_cnt); //좌표 확인
 
 	}
 
