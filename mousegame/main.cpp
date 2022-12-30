c3884e871b56c7bc59579733c64faf46e435f075
hyeg <hyeg0121@naver.com>
2022-12-30 13:30:58 +0900

    마우스 좌표 확인

--- mousegame/main.cpp
+++ mousegame/main.cpp
@@ -1,7 +1,34 @@
-#include<stdio.h>
-#include<SFML/Graphics.hpp>
-#include<SFML/Audio.hpp>
+#include <stdio.h>
+#include <SFML/Graphics.hpp>
+#include <stdlib.h>
+#include <time.h>
+#include <SFML/Audio.hpp>
+
+using namespace sf;
 
 int main(void) {
+	RenderWindow window(VideoMode(1200, 800), "AfterSchool2");
+	window.setFramerateLimit(60);
+
+	Vector2i mouse_pos;
+
+	while (window.isOpen())
+	{
+		mouse_pos = Mouse::getPosition();
+		Event event;
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+			case Event::Closed: 
+				window.close();
+				break;
+			}//switch
+		}//event
+
+		printf("(%d, %d)\n", mouse_pos.x, mouse_pos.y);
+
+	}
+
 	return 0;
 }
